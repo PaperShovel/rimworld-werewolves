b318f58f597bcfb39cc912b938893877120a4a59
PaperShovel <stech.ben@gmail.com>
2023-03-06 11:39:56 -0500

    Test runs of initial setup

---
+++ Source/Rimworld Werewolves/Werewolf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Rimworld_Werewolves
+{
+    [StaticConstructorOnStartup]
+    public static class Werewolf
+    {
+        static Werewolf()
+        {
+            Log.Message("Werewolf!");
+        }
+    }
+}
